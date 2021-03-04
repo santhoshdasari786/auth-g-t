6c092f18275f11968791b5e9226588b5d977916e
Santhosh <santhoshdasari786@gmail.com>
2021-03-04 14:05:36 +0530

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import Application from "./pages/functionalcomponents/Application";
 import UserProvider from "./providers/UserProvider";
 
